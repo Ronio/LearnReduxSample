674f638854382883069a5df42184d526cf878e5f
Ronio <Ronio.hu@gmail.com>
2017-12-11 01:29:00 +0800

    完成后的代码

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts', pages: 39 },
+        { title: 'Harry Potter', pages: 19},
+        { title: 'The Dark Tower', pages: 302 },
+        { title: 'Eloquent Ruby', pages: 123 }
+    ]
+}
